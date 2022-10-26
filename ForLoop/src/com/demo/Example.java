53c832d3e327b1373bf2429c29a341fc2445e96d
Ankan <Ankan@DESKTOP-DIE3IND>
2022-10-26 00:38:08 -0700

    For Loop

---
+++ ForLoop/src/com/demo/Example.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Example {
+
+	static void print() {
+		for (int i = 0; i <= 10; i++) {
+
+			System.out.println(i);
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		print();
+	}
+}
